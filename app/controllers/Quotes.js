ae56788cce6969f9276e971151a1c9be0cb19617
J. Steve Martinez <j.steve.martinez@gmail.com>
2017-03-13 13:46:51 -0700

    enable real data

--- app/controllers/Quotes.js
+++ app/controllers/Quotes.js
@@ -14,38 +14,38 @@ function Quotes() {
             /**
              * Use saved twtr data for dev
              */
-            res.send(data);
+            // res.send(data);
 
             /**
              * Remove yahooFinance call until client is working better
              * this is so i don't hammer yahoo will tons of calls and get 
              * my IP blocked for spamming the server
              */
-            // var data = JSON.parse(body);
-            // var dates = data.dates;
+            var data = JSON.parse(body);
+            var dates = data.dates;
 
-            // console.log(data);
-            // yahooFinance.historical({
-            //     symbols: data.symbols,
-            //     from: dates.start,
-            //     to: dates.end,
-            // }).then(result => {
-            //     // if (err) { throw err; }
-            //     console.log('quote results');
-            //     // console.log(result);
+            console.log(data);
+            yahooFinance.historical({
+                symbols: data.symbols,
+                from: dates.start,
+                to: dates.end,
+            }).then(result => {
+                // if (err) { throw err; }
+                console.log('quote results');
+                // console.log(result);
 
-            //     /**
-            //      * This was to get mock data and save to a file
-            //      * remove when client is finished
-            //      */
-            //     // fs.writeFile("data.json", JSON.stringify(result), function (err) {
-            //     //     if (err) {
-            //     //         return console.log(err);
-            //     //     }
-            //     // });
-            //     res.send(result);
+                /**
+                 * This was to get mock data and save to a file
+                 * remove when client is finished
+                 */
+                // fs.writeFile("data.json", JSON.stringify(result), function (err) {
+                //     if (err) {
+                //         return console.log(err);
+                //     }
+                // });
+                res.send(result);
 
-            // });
+            });
         });
     }
 
